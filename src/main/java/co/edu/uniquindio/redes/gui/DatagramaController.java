d905178ec1776a1bfef29e6468825d40c695a6f9
Sandra Milena Quintero Leal <smquinterol@uqvirtual.edu.co>
2021-11-29 22:02:21 -0500

    se adiciona método para cargar archivo de texto con datagrama

--- src/main/java/co/edu/uniquindio/redes/gui/DatagramaController.java
+++ src/main/java/co/edu/uniquindio/redes/gui/DatagramaController.java
@@ -3,13 +3,13 @@ package co.edu.uniquindio.redes.gui;
 
 import co.edu.uniquindio.redes.model.DHCPMessage;
 import co.edu.uniquindio.redes.model.dhcp.options.DHCPOption;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
 import co.edu.uniquindio.redes.util.ByteUtils;
-import co.edu.uniquindio.redes.model.DataFrame;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,31 +22,31 @@ public class DatagramaController {
     private TextArea resultadoText;
 
     @FXML
-    private void  leerDatagrama(){
+    private void leerDatagrama() {
 
-        byte[] data= ByteUtils.stringToByteArray(datagramaText.getText());
+        byte[] data = ByteUtils.stringToByteArray(datagramaText.getText());
 
 
         DHCPMessage dhcpMessage = new DHCPMessage(data);
 
         StringBuilder resultado = new StringBuilder();
 
-        resultado.append( "Dirección MAC destino: "+ dhcpMessage.getDestinationMacAddress() +
-                "\nDirección MAC origen: "+ dhcpMessage.getSourceMacAddress()+
-                "\nDirección IP origen: "+dhcpMessage.getSourceAddressIP()+
-                "\nDirección IP destino: "+dhcpMessage.getDestinationAddressIP()+
-                "\nNúmero de puerto origen: "+dhcpMessage.getSourcePort()+
-                "\nNúmero de puerto destino: "+dhcpMessage.getDestinationPort()+
-                "\nLongitud del segmento UDP: "+dhcpMessage.getUDPSegmentLength()+
-                "\nLongitud del mensaje DHCP: "+dhcpMessage.getLength()+
-                "\nTipo mensaje DHCP: "+dhcpMessage.getMessageType()+
-                "\nTipo hardware de red: "+dhcpMessage.getHardwareType()+
-                "\nTamaño de la dirección de hardware: "+dhcpMessage.getHlen()+
-                "\nIdentificador de la transacción: "+dhcpMessage.getXidString()+
-                "\nDirección IP cliente: "+dhcpMessage.getClientAddressIPString()+
-                "\nSu dirección IP: "+dhcpMessage.getSuAddressIPString()+
-                "\nDirección Hardware Cliente: "+dhcpMessage.getHardwareClientAddressString()+
-                "\nMagic Cokkie: "+dhcpMessage.getMagicCokkie());
+        resultado.append("Dirección MAC destino: " + dhcpMessage.getDestinationMacAddress() +
+                "\nDirección MAC origen: " + dhcpMessage.getSourceMacAddress() +
+                "\nDirección IP origen: " + dhcpMessage.getSourceAddressIP() +
+                "\nDirección IP destino: " + dhcpMessage.getDestinationAddressIP() +
+                "\nNúmero de puerto origen: " + dhcpMessage.getSourcePort() +
+                "\nNúmero de puerto destino: " + dhcpMessage.getDestinationPort() +
+                "\nLongitud del segmento UDP: " + dhcpMessage.getUDPSegmentLength() +
+                "\nLongitud del mensaje DHCP: " + dhcpMessage.getLength() +
+                "\nTipo mensaje DHCP: " + dhcpMessage.getMessageType() +
+                "\nTipo hardware de red: " + dhcpMessage.getHardwareType() +
+                "\nTamaño de la dirección de hardware: " + dhcpMessage.getHlen() +
+                "\nIdentificador de la transacción: " + dhcpMessage.getXidString() +
+                "\nDirección IP cliente: " + dhcpMessage.getClientAddressIPString() +
+                "\nSu dirección IP: " + dhcpMessage.getSuAddressIPString() +
+                "\nDirección Hardware Cliente: " + dhcpMessage.getHardwareClientAddressString() +
+                "\nMagic Cokkie: " + dhcpMessage.getMagicCokkie());
 
         resultado.append("\nOpciones: \n");
         dhcpMessage.getOptions().stream().map(DHCPOption::toString).forEach(resultado::append);
@@ -55,16 +55,23 @@ public class DatagramaController {
 
 
     }
-/*
+
     @FXML
-    private void  leerArchivo() {
-
-        Path path = Paths.get(args[0]);
-        data = Files.readAllBytes(path);
-        String texto = new String(data);
-
+    private void leerArchivo() {
+        try {
+            FileChooser fileChooser = new FileChooser();
+            File file = fileChooser.showOpenDialog(HelloApplication.getStage());
+            if( file != null ) {
+                Path path = file.toPath(); //Paths.get(args[0]);
+                byte[] data = new byte[0];
+                data = Files.readAllBytes(path);
+                String texto = new String(data);
+                datagramaText.setText(texto);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-      */
 
-    }
+}
--- src/main/java/co/edu/uniquindio/redes/gui/HelloApplication.java
+++ src/main/java/co/edu/uniquindio/redes/gui/HelloApplication.java
@@ -8,13 +8,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Stage stage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        HelloApplication.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/fxml/datagrama-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1010, 620);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static Stage getStage(){
+        return stage;
     }
 
     public static void main(String[] args) {
